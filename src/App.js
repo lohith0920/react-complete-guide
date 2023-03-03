55c22c548bf558f1e12659dcb0014f11f35e6ab2
lohithcostrategix <lohith.reddy@costrategix.com>
2023-03-04 00:34:49 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import ExpenseItem from './components/ExpenseItem';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's Stat</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
